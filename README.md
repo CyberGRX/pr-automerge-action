ba9939658ee2fb2a566eca5a58dde3a4db291b82
Dean Kooiman <dean.kooiman@cybergrx.com>
2021-08-05 21:02:47 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PR AutoMerge Management Javascript Action
+This action manages the auto-merge state for a PR using labels.
---
+++ index.ts
@@ -0,0 +1,125 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as format from 'string-format';
+import { graphql } from '@octokit/graphql';
+import {
+  WebhookEventMap,
+  PullRequestEvent,
+  PullRequest,
+} from '@octokit/webhooks-types';
+import { titleCase } from 'title-case';
+
+const enableAutoMergeMutation = `mutation enableAutoMerge($pullRequestId: ID!, $strategy: PullRequestMergeMethod) {
+  enablePullRequestAutoMerge(input: {pullRequestId: $pullRequestId, mergeMethod: $strategy}) {
+    pullRequest {
+      id
+      autoMergeRequest {
+        enabledAt
+        mergeMethod
+      }
+    }
+  }
+}`;
+
+const disableAutoMergeMutation = `mutation disableAutoMerge($pullRequestId: ID!) {
+  disablePullRequestAutoMerge(input: {pullRequestId: $pullRequestId}) {
+    pullRequest {
+      id
+      autoMergeRequest {
+        enabledAt
+        mergeMethod
+      }
+    }
+  }
+}`;
+
+const getPullRequestQuery = `query getPullRequest($owner: String!, $repo: String!, $number: Int!) {
+  repository(name: $repo, owner: $owner) {
+    pullRequest(number: $number) {
+      autoMergeRequest {
+        mergeMethod
+      }
+    }
+  }
+}
+`;
+
+declare type AutoMergeVariables = {
+  pullRequestId: String;
+  strategy?: String;
+};
+
+declare type PullRequestVariables = {
+  owner: String;
+  repo: String;
+  number: Number;
+};
+
+async function run() {
+  try {
+    const token = process.env['GITHUB_TOKEN'];
+    if (!token) {
+      core.setFailed('GITHUB_TOKEN does not exist.');
+      return;
+    }
+
+    const graphqlWithAuth = graphql.defaults({
+      headers: { authorization: `token ${token}` },
+    });
+
+    const setAutoMerge = async (
+      pullRequest: PullRequest,
+      enable: boolean,
+      strategy: string,
+    ) => {
+      try {
+        const query = enable
+          ? enableAutoMergeMutation
+          : disableAutoMergeMutation;
+        const variables: AutoMergeVariables = {
+          pullRequestId: pullRequest.node_id,
+        };
+        if (enable) {
+          variables.strategy = strategy;
+        }
+        const result = await graphqlWithAuth(query, variables);
+        const response = JSON.stringify(result, undefined, 2);
+        console.log(`The response payload: ${response}`);
+      } catch (error) {
+        console.log(`Request failed: ${JSON.stringify(error)}`);
+      }
+    };
+
+    const getPullRequest = async () => {
+      const variables: PullRequestVariables = {
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        number: github.context.payload.pull_request.number,
+      };
+      try {
+        const result = await graphqlWithAuth(getPullRequestQuery, variables);
+        const response = JSON.stringify(result, undefined, 2);
+        console.log(`The response payload: ${response}`);
+      } catch (error) {
+        console.log(`Request failed: ${JSON.stringify(error)}`);
+      }
+    };
+
+    const label = core.getInput('label');
+    const strategy = core.getInput('strategy');
+
+    await getPullRequest();
+
+    const payload = github.context.payload as PullRequestEvent;
+    const pr = payload.pull_request;
+
+    const found = pr.labels.find(l => l.name === label);
+    if (!found) {
+      console.log('Not found');
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
